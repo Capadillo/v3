b4759f1c90356e42ab441829d5cc94684e098b55
Capadillo <joshua.capes@live.com>
2024-02-11 22:12:14 +1100

    added service worker

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register("../service.js");
+}
+
 // =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
 // Tabs & Panels
 // =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
