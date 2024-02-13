f4f42203294b20e3487f381be2d7e7ff20ba5180
Capadillo <joshua.capes@live.com>
2024-02-13 20:55:26 +1100

    more work on the panels

--- js/app.js
+++ js/app.js
@@ -1,137 +1,76 @@
+import { Cookie } from './Cookie.js';
+
+// =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+// Service Worker
+// =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register("../service.js");
 }
 
 // =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
-// Tabs & Panels
+// Accordian
 // =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
 
-function serialize(obj) {
-  var str = [];
-  for (var p in obj)
-    if (obj.hasOwnProperty(p)) {
-      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-    }
-  return str.join(";");
-}
-
-function deserialize(str) {
-    let obj = {};
-    
-    for (var p in str.split(";")) {
-        var { key, value } = p.split('=');
-        obj[key] = value;
-    }
-
-    return obj;
-}
-
-class Cookie {
-  static get(name) {
-    const cookieName = `${encodeURIComponent(name)}=`;
-    const cookie = document.cookie;
-    let value = null;
-
-    const startIndex = cookie.indexOf(cookieName);
-    if (startIndex > -1) {
-      let endIndex = cookie.indexOf(';', startIndex);
-      if (endIndex == -1) {
-        endIndex = cookie.length;
-      }
-      value = decodeURIComponent(
-        cookie.substring(startIndex + name.length + 1, endIndex)
-      );
-    }
-    return value;
-  }
-
-  static set(name, value, expires, path, domain, secure) {
-    let cookieText = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`;
-    if (expires instanceof Date) {
-      cookieText += `; expires=${expires.toGMTString()}`;
-    }
-
-    if (path) cookieText += `; path=${path}`;
-    if (domain) cookieText += `; domain=${domain}`;
-    if (secure) cookieText += `; secure`;
-
-    document.cookie = cookieText;
-  }
-
-  static remove(name, path, domain, secure) {
-    Cookie.set(name, '', new Date(0), path, domain, secure);
-  }
-}
-
-let user_interacted = [];
-
-$(document).ready(function() {
-  $('[data-accordian] + li').addClass('hidden');
-  $('[data-accordian]').on('click', function() {
+$('[data-accordian]').on('click', function() {
     const group = $(this).data('accordian');
     const parent = $(this);
-    $(`[data-accordian=${group}]`).each(function() {
-      if ($(this)[0] === parent[0] && $(this).next('li').is(':visible') == false) {
-        $(this).next('li').removeClass('hidden');
-      } else {
-        $(this).next('li').addClass('hidden');
-      }
-    });
-  });
-    //$('#dialog__editor')[0].showModal();
-    // hide all panels
-    $(`main [id^="panel__"]`).hide();
     
-    let active = Cookie.get('active') ?? 'attributes';
-
-    $(`[data-panel="${active}"]`).addClass('active');
-    $(`#panel__${active}`).show();
-
-    const width = $(window).width();
-
-    $('ul[collapsable]').each(function() {
-        if (width <= 512) {
-            $(this).addClass('collapsed');
+    $(`[data-accordian=${group}]`).each(function() {
+        if ($(this)[0] === parent[0] && $(this).next('li').is(':visible') == false) {
+            $(this).next('li').removeClass('hidden');
+        } else {
+            $(this).next('li').addClass('hidden');
         }
     });
 });
 
-$('#nav__main > *').click(function() {
-    const panel = $(this).data('panel');
-    Cookie.set('active', panel);
+$(function() {
+    $('[data-accordian] + li').addClass('hidden');
+});
 
-    // swap the active class on the tabs
-    $('#nav__main .active').removeClass('active');
+// =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+// Collapsable
+// =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+
+let user_clicked = [];
+
+$('ul[collapsable]').on('click', function() {
+    if (!user_clicked.includes(this)) {
+        user_clicked.push(this);
+    }
+
+    $(this).toggleClass('collapsed');
+});
+
+// =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+// Panel
+// =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+
+$('[data-panel]').on('click', function() {
+    const panel = $(this).data('panel');
+
+    // store the last clicked panel in cookies
+    Cookie.set('active_panel', panel);
+
+    // toggle the active panel on the navbar
+    $(`[data-panel]`).removeClass('active');
     $(this).addClass('active');
 
-    $(`main [id^="panel__"]`).hide();
-    $(`#panel__${panel}`).show();
-});
+    // toggle the panel to be displayed
+    $(`main [id*=panel__]`).not(`#panel__${panel}`).addClass('hidden');
+    $(`#panel__${panel}`).removeClass('hidden');
+})
 
-$('ul[collapsable] .heading').click(function() {
-    const ul = $(this).parent('ul');
+$(function() {
+    // get the active panel from cookies or default
+    const panel = Cookie.get('active_panel') ?? 'attributes';
 
-    user_interacted.push(ul[0]);
+    // toggle the active panel on the navbar
+    $(`[data-panel]`).removeClass('active');
+    $(`[data-panel='${panel}']`).addClass('active');
 
-    if ($(ul).hasClass('collapsed')) {
-        $(ul).removeClass('collapsed');
-    } else {
-        $(ul).addClass('collapsed');
-    }
-});
-
-$(window).on('resize', function() {
-    const width = $(this).width();
-    
-    $('ul[collapsable]').each(function() {
-        if (user_interacted.includes(this)) {
-            return;
-        }
-
-        if (width <= 512) {
-            $(this).addClass('collapsed');
-        } else {
-            $(this).removeClass('collapsed');
-        }
-    });
-});
+    // toggle the panel to be displayed
+    $(`main [id*=panel__]`).not(`#panel__${panel}`).addClass('hidden');
+    $(`#panel__${panel}`).removeClass('hidden');
+})
